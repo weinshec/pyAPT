dd88feda54e93000b4cced8ead0289c2c362ff7b
Christoph Weinsheimer <weinshec@students.uni-mainz.de>
2015-05-13 20:55:10 +0200

    Reviewd all pyAPT classes and adapted syntax and docstrings

--- pyAPT/__init__.py
+++ pyAPT/__init__.py
@@ -1,44 +1,59 @@
-__version__ = "0.01"
-__author__ = "Shuning Bian"
+"""
+pyAPT - python interface to Thorlabs APT motion controllers
 
-__all__ = ['Message', 'Controller', 'MTS50', 'add_PID', 'clear_PIDs', 'OutOfRangeError', 'PRM1']
+This is a fork of the pyAPT module written by Shuning Bian that I ported to
+Python 3 and customized it to my needings. All the credits for the original
+module go to him.
+"""
 
-from pyAPT import message, controller, mts50, prm1
+__author__  = "Christoph Weinsheimer"
+__email__   = "christoph.weinsheimer@desy.de"
+__version__ = "0.2"
 
-Message = message.Message
-Controller = controller.Controller
-MTS50 = mts50.MTS50
-PRM1 = prm1.PRM1
+
+__all__ = ['Message', 'Controller', 'add_PID', 'clear_PIDs', 'OutOfRangeError']
+
+
+from pyAPT import message, controller
+
+Message         = message.Message
+Controller      = controller.Controller
 OutOfRangeError = controller.OutOfRangeError
+
 
 import pylibftdi
 
 def add_PID(pid):
-  """
-  Adds a USB PID to the list of PIDs to look for when searching for APT
-  controllers
-  """
-  pylibftdi.USB_PID_LIST.append(pid)
+    """Add USB PID to the libftdi list
+
+    Adds a USB PID to the list of PIDs to look for when searching for APT
+    controllers
+
+    Args:
+        pid (byte): pid to add to the list
+    """
+    pylibftdi.USB_PID_LIST.append(pid)
+
 
 def clear_PIDs():
-  """
-  Clears all USB PIDs
-  """
-  l = pylibftdi.USB_PID_LIST
-  while len(l):
-    l.pop()
+    """Clears all USB PIDs
+    """
+    l = pylibftdi.USB_PID_LIST
+    while len(l):
+        l.pop()
 
-# XXX By default pylibftdi looks for devices with PID of 0x6001 and 0x6014
-# which slows device listing and identification down when we JUST want to
-# identify motion controllers. So we do this little dance here.
-#
-# If you are using a single class of controllers, just replace 0xfaf0 with
-# the controller's PID. If more than one class is being used, add each class's
-# PID.
-#
-# Note that we cannot simply do pylibftdi.USB_PID_LIST = [...] because that
-# just modifies pylibftdi.USB_PID_LIST, not the list used by driver.py in the
-# pylibftdi package.
+"""
+By default pylibftdi looks for devices with PID of 0x6001 and 0x6014 which
+slows device listing and identification down when we JUST want to identify
+motion controllers. So we do this little dance here.
+
+If you are using a single class of controllers, just replace 0xfaf0 with the
+controller's PID. If more than one class is being used, add each class's PID.
+
+Note that we cannot simply do pylibftdi.USB_PID_LIST = [...] because that just
+modifies pylibftdi.USB_PID_LIST, not the list used by driver.py in the
+pylibftdi package.
+"""
 
 clear_PIDs()
 add_PID(0xFAF0)
--- pyAPT/mts50.py
+++ pyAPT/mts50.py
@@ -1,31 +1,41 @@
+"""
+MTS50 - controller subclass for the Thorlabs MTS50/M-Z8 stage
+"""
+
 from .controller import Controller
 
 class MTS50(Controller):
-  """
-  A controller for a MTS50/M-Z8 stage.
-  """
-  def __init__(self,*args, **kwargs):
-    super(MTS50, self).__init__(*args, **kwargs)
+    """
+    MTS50 - controller subclass for the Thorlabs MTS50/M-Z8 stage
+    """
 
-    # http://www.thorlabs.co.uk/thorProduct.cfm?partNumber=MTS50/M-Z8
-    # Note that these values are pulled from the APT User software,
-    # as they agree with the real limits of the stage better than
-    # what the website or the user manual states
-    self.max_velocity = 0.45
-    self.max_acceleration = 0.45
+    def __init__(self,*args, **kwargs):
+        """Constructor
 
-    # from private communication with thorlabs tech support:
-    # steps per revolution: 48
-    # gearbox ratio: 256
-    # pitch: 0.5 mm
-    # thus to advance 1 mm you need to turn 48*256*2 times
-    enccnt = 48*256*2
-    T = 2048/6e6
+        Passes *args, **kwargs to Controller.Constructor
+        """
 
-    # these equations are taken from the APT protocol manual
-    self.position_scale = enccnt
-    self.velocity_scale = enccnt * T * 65536
-    self.acceleration_scale = enccnt * T * T * 65536
-
-    self.linear_range = (0,50)
+        super(MTS50, self).__init__(*args, **kwargs)
+     
+        # http://www.thorlabs.co.uk/thorProduct.cfm?partNumber=MTS50/M-Z8
+        # Note that these values are pulled from the APT User software,
+        # as they agree with the real limits of the stage better than
+        # what the website or the user manual states
+        self.max_velocity = 0.45
+        self.max_acceleration = 0.45
+     
+        # from private communication with thorlabs tech support:
+        # steps per revolution: 48
+        # gearbox ratio: 256
+        # pitch: 0.5 mm
+        # thus to advance 1 mm you need to turn 48*256*2 times
+        enccnt = 48*256*2
+        T = 2048/6e6
+     
+        # these equations are taken from the APT protocol manual
+        self.position_scale = enccnt
+        self.velocity_scale = enccnt * T * 65536
+        self.acceleration_scale = enccnt * T * T * 65536
+     
+        self.linear_range = (0,50)
 
