9220523e6bcac6b80410a099b2f2fd30d7cbb7d3
Christoph Weinsheimer <weinshec@students.uni-mainz.de>
2015-06-02 10:04:00 +0200

    Add first draft of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name             = 'pyAPT',
+    version          = '0.1.0',
+    author           = 'Christoph Weinsheimer',
+    author_email     = 'christoph.weinsheimer@desy.de',
+    packages         = ['pyAPT'],
+    scripts          = [],
+    description      = 'Controller module for Thorlabs motorized stages',
+    install_requires = [],
+)
